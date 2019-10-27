321d96dd5fb9d3561d4dc16123b7c8713a03cefd
ZhangLie012 <1309427117@qq.com>
2019-10-27 21:32:37 +0800

    增加tlb更新，缺页处理

--- code/userprog/exception.cc
+++ code/userprog/exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //	Entry point into the Nachos kernel from user programs.
 //	There are two kinds of things that can cause control to
 //	transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //	exceptions -- The user code does something that the CPU can't handle.
 //	For instance, accessing memory that doesn't exist, arithmetic errors,
-//	etc.  
+//	etc.
 //
 //	Interrupts (which can also cause control to transfer from user
 //	code into the Nachos kernel) are handled elsewhere.
@@ -18,7 +18,7 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
@@ -39,40 +39,99 @@
 //		arg3 -- r6
 //		arg4 -- r7
 //
-//	The result of the system call, if any, must be put back into r2. 
+//	The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to increment the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//	"which" is the kind of exception.  The list of possible exceptions 
+//	"which" is the kind of exception.  The list of possible exceptions
 //	are in machine.h.
 //----------------------------------------------------------------------
 static unsigned int nextTlbIndex=0;
-void demandingPage(int virtAddr){
-	
+static unsigned int clockIndex=0;
+static unsigned int nextExchangedPhyPage=0;
+extern BitMap bitMap;
+void WriteBackToDisk(int virtAddr){
+
 }
 
-void 
+
+int FindNextTlbIndex(){
+	printf("更新tlb");
+	/*
+	int i=0;
+	for(i=0;i<TLBSize;++i){
+		if(!machine->tlb[clockIndex].use){
+			break;
+		}
+		clockIndex++;
+		clockIndex%=TLBSize;
+	}
+	if(i==TLBSize){
+		for(i=0;i<TLBSize;++i){
+			if(!machine->tlb[clockIndex].dirty){
+				break;
+			}
+			clockIndex++;
+			clockIndex%=TLBSize;
+		}
+	}
+	if(machine->tlb[clockIndex].dirty){
+		WriteBackToDisk(machine->tlb[clockIndex].virtualPage);
+	}
+	int nextIndex=clockIndex;
+	clockIndex++;
+	clockIndex%=TLBSize;
+	*/
+	nextTlbIndex%=TLBSize;
+	return nextTlbIndex++;
+}
+
+void DemandingPage(int virtAddr){
+	printf("请求调页\n");
+	int vpn = (unsigned) virtAddr / PageSize;
+	int nextPhysPage=bitMap.Find();
+	if(nextPhysPage==-1){
+		nextPhysPage=nextExchangedPhyPage++;
+		nextExchangedPhyPage%=NumPhysPages;
+	}
+	machine->pageTable[vpn].physicalPage = nextPhysPage;
+	machine->pageTable[vpn].valid = TRUE;
+	machine->pageTable[vpn].use = TRUE;
+	machine->pageTable[vpn].dirty = FALSE;
+	machine->pageTable[vpn].readOnly = FALSE;
+	int nextIndex=FindNextTlbIndex();
+	machine->tlb[nextIndex]=machine->pageTable[vpn];
+	return ;
+}
+
+void
 PageFaultHandler(){
+		printf("%s\n","处理缺页错误");
 		int virtAddr = machine->registers[PCReg];
 		int vpn = (unsigned) virtAddr / PageSize;
 		TranslationEntry *currentPageTable = machine->pageTable;
 		//如果也页表中有相应的物理页，则更新tlb即可，否则请求调页。
 		if(currentThread->space != NULL && machine->tlb != NULL && currentPageTable[vpn].valid){
-			// tlb FIFO
+			/* tlb 先进先出置换算法
 			machine->tlb[nextTlbIndex]=currentPageTable[vpn];
 			nextTlbIndex++;
 			nextTlbIndex%=TLBSize;
+			*/
+			//TLB 时钟置换算法
+			int nextIndex=FindNextTlbIndex();//与先进先出的nextTlbIndex不一样。
+			machine->tlb[nextIndex]=currentPageTable[vpn];
 			return ;
 		}
 		else{
-			demandingPage(virtAddr);
-		}	
+			DemandingPage(virtAddr);
+			return ;
+		}
 }
 void
 ExceptionHandler(ExceptionType which)
 {
-    int type = machine->ReadRegister(2);
+  int type = machine->ReadRegister(2);
 	switch(which){
 		case SyscallException:
 			if(type==SC_Halt){
@@ -88,11 +147,4 @@ ExceptionHandler(ExceptionType which)
 			PageFaultHandler();
 			break;
 	}
-    if ((which == SyscallException) && (type == SC_Halt)) {
-	DEBUG('a', "Shutdown, initiated by user program.\n");
-   	interrupt->Halt();
-    } else {
-	printf("Unexpected user mode exception %d %d\n", which, type);
-	ASSERT(FALSE);
-    }
 }
